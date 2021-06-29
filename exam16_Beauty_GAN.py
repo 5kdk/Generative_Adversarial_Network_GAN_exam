a35e15450a2371dba67392686a6cc22b975152cf
scolpig <scolpig@gmail.com>
2021-06-29 16:10:21 +0900

    완료

--- exam16_Beauty_GAN.py
+++ exam16_Beauty_GAN.py
@@ -1,18 +1,12 @@
 import dlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-#import tensorflow._api.v2 as tf # 2.x 버전용 코드
-import tensorflow.compat.v1 as tf 
+import tensorflow.compat.v1 as tf
+# import tensorflow._api.v2 as tf # 2.x 버전용 코드
 tf.disable_v2_behavior() # tensorflow 2.x 버젼에서 1.x버젼 코드가 실행되도록
 import numpy as np
 
-detector = dlib.get_frontal_face_detector()
-sp = dlib.shape_predictor('./models/shape_predictor_5_face_landmarks.dat')
-    
-img = dlib.load_rgb_image(('./imgs/12.jpg'))
-plt.figure(figsize=(16,10))
-plt.imshow(img)
-plt.show()
+def align_faces(img, detector, sp):
 
 # face_detector으로 얼굴 인식
 img_result = img.copy()
@@ -43,9 +37,23 @@ for detection in dets:
 ax.imshow(img_result)
 plt.show()
 
-faces = dlib.get_face_chips(img,objs, size=256, padding=0.3)
-fig, axes = plt.subplot(1, len(faces)+1, figsize=(20,16))
-axes[0].imshow(img)
-for i, face in enumerate(faces):
-    axes[i+1].imshow(face)
+src_img = img1_faces[0]
+ref_img = img2_faces[0]
+
+X_img = preprocess(src_img)
+X_img = np.expand_dims(X_img, axis=0)
+
+Y_img = preprocess(ref_img)
+Y_img = np.expand_dims(Y_img, axis=0)
+
+output = sess.run(Xs, feed_dict={X:X_img, Y:Y_img})
+output_img = deprocess(output[0])
+
+fig, axes = plt.subplots(1,3,figsize=(20,10))
+axes[0].set_title('Source')
+axes[0].imshow(src_img)
+axes[1].set_title('Reference')
+axes[1].imshow(ref_img)
+axes[2].set_title('Result')
+axes[2].imshow(output_img)
 plt.show()
